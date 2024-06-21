3c26359c0cf64602ea6c930d6c97d0b894b110b2
Hilton-Borah <hiltonborah123@gmail.com>
2024-06-21 17:30:42 +0530

    updated bugs

--- src/Pages/dashboard/sessiondetails.jsx
+++ src/Pages/dashboard/sessiondetails.jsx
@@ -20,41 +20,62 @@ const Sessiondetails = () => {
     if (!singleSession?.activity?.length) {
       return <div>No activity present</div>;
     }
-    return singleSession.activity.map((el) => (
-      <li key={el._id}>{el.name}</li>
-    ));
+    return singleSession.activity.map((el,index) => {
+      return (
+        <div key={el._id}>
+          {index + 1}.{" "}{el.name}
+        </div>
+      );
+    });
   };
 
   const renderParticipants = () => {
     if (!singleSession?.participants?.length) {
-      return <li>No participant present</li>;
+      return <div>No participant present</div>;
     }
-    return singleSession.participants.map((el) => {
-      const participant = partcipantList.find((pl) => pl._id === el.participantId);
+    return singleSession.participants.map((el,index) => {
+      const participant = partcipantList.find(
+        (pl) => pl._id === el.participantId
+      );
       return participant ? (
-        <li key={participant._id}>{participant.name}</li>
+        <div key={participant._id}>
+          {index + 1}.{" "}
+          {participant.name}
+        </div>
       ) : null;
     });
   };
 
   return (
-    <div className="">
-      <div><b>Session name:</b> {singleSession?.name || "N/A"}</div>
-      <div><b>Cohort name:</b> {singleSession?.cohort?.name || "N/A"}</div>
-      <div><b>Date of session:</b> {singleSession?.date?.split("T")[0] || "N/A"}</div>
-      <div><b>No. of hours:</b> {singleSession?.numberOfHours || "N/A"}</div>
-      <div>
-      <span className="font-bold"> Activities: </span>
-        <ul className="list-disc ml-5">
-          {renderActivities()}
-        </ul>
-      </div>
-      <div>
-        <span className="font-bold">All attended participants:</span>
-        <ul className="list-disc ml-5">
-          {renderParticipants()}
-        </ul>
-      </div>
+    <div className="mt-[20px]">
+      <table className="min-w-full divide-y divide-gray-200">
+        <tbody>
+          <tr>
+            <td className="py-2 font-semibold">Session name</td>
+            <td>{singleSession?.name || "N/A"}</td>
+          </tr>
+          <tr>
+            <td className="py-2 font-semibold">Cohort name</td>
+            <td>{singleSession?.cohort?.name || "N/A"}</td>
+          </tr>
+          <tr>
+            <td className="py-2 font-semibold">Date of session</td>
+            <td>{singleSession?.date?.split("T")[0] || "N/A"}</td>
+          </tr>
+          <tr>
+            <td className="py-2 font-semibold">No. of hours</td>
+            <td> {singleSession?.numberOfHours || "N/A"}</td>
+          </tr>
+          <tr>
+            <td className="py-2 font-semibold">Activities</td>
+            <td> {renderActivities()}</td>
+          </tr>
+          <tr>
+            <td className="py-2 font-semibold">All attended participants</td>
+            <td> {renderParticipants()}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
