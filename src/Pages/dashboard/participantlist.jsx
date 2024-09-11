69491f7d36c09516c61ea6714a1360ae308dbcdc
Hilton-Borah <hiltonborah123@gmail.com>
2024-09-11 16:32:41 +0530

    latest code

--- src/Pages/dashboard/participantlist.jsx
+++ src/Pages/dashboard/participantlist.jsx
@@ -4,11 +4,14 @@ axios.defaults.withCredentials = true;
 import { useEffect, useState } from "react";
 import { serverUrl } from "../../api";
 import { CiEdit } from "react-icons/ci";
-import { MdOutlineDeleteOutline } from "react-icons/md";
+import { MdOutlineDeleteOutline, MdOutlineRemoveRedEye } from "react-icons/md";
 import EditParticipants from "../../Componants/EditParticipants";
 import { Link, useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllParticipants, getParticipantsByName } from "../../Redux/AllListReducer/action";
+import {
+  getAllParticipants,
+  getParticipantsByName,
+} from "../../Redux/AllListReducer/action";
 import { RiArrowLeftSLine, RiArrowRightSLine } from "react-icons/ri";
 import { convertDateFormat } from "../../Utils/localStorage";
 
@@ -18,40 +21,28 @@ export const Participantlist = () => {
   const [singleParticipant, setSinglePartcipant] = useState({});
   const [currentPage, setCurrentPage] = useState(searchParams.get("page") || 1);
   const [limit, setLimit] = useState(searchParams.get("limit") || 10); // default limit
-  const [searchResult, setSearchResult] = useState("")
+  const [searchResult, setSearchResult] = useState("");
   const dispatch = useDispatch();
+  const [name, setName] = useState("");
 
   const toggleModal = (el) => {
-    // setSearchParams({ id: el._id });
+    setName("");
     setIsModalOpen(!isModalOpen);
     setSinglePartcipant(el);
   };
 
-  const tableHead = [
-    "Name",
-    "Email",
-    "Type",
-    "Gender",
-    "Centre",
-    "Date of birth",
-    "Address",
-    "Mobile no",
-    "State",
-    "City",
-    "Pincode",
-    "Emg. name",
-    "Emg. contact no.",
-    "Emg. relationship",
-    "Created date",
-    "",
-    "",
-    "",
-  ];
+  const toggleModalRead = (el) => {
+    setName("read");
+    setIsModalOpen(!isModalOpen);
+    setSinglePartcipant(el);
+  };
 
-  const { partcipantList,cohortList } = useSelector((state) => {
+  const tableHead = ["Name", "Email", "Type", "Gender", "Centre", "", ""];
+
+  const { partcipantList, cohortList } = useSelector((state) => {
     return {
       partcipantList: state.AllListReducer.partcipantList,
-      cohortList : state.AllListReducer.cohortList
+      cohortList: state.AllListReducer.cohortList,
     };
   });
 
@@ -66,36 +57,48 @@ export const Participantlist = () => {
     });
   }, [currentPage, limit]);
 
-  const handleSearchSubmit = (e)=>{
+  const handleSearchSubmit = (e) => {
     e.preventDefault();
-    dispatch(getParticipantsByName(searchResult))
-      }  
-
+    dispatch(getParticipantsByName(searchResult));
+  };
 
   return (
     <Card className="h-full w-full overflow-scroll mt-5 mb-24">
       <div className="flex justify-between items-center gap-5 mt-4 mr-3 ml-3">
         <div className="w-[50%]">
-          <form className="flex justify-start items-center gap-5" onSubmit={handleSearchSubmit}>
+          <form
+            className="flex justify-start items-center gap-5"
+            onSubmit={handleSearchSubmit}
+          >
             <div className="w-[50%]">
-            <Input
-              label="Search member name..."
-              name="password"
-              // type="search"
-              required
-              value={searchResult}
-              // type={showPassword ? "text" : "password"}
-              onChange={(e) => setSearchResult(e.target.value)}
-            />
-
+              <Input
+                label="Search member name..."
+                name="password"
+                // type="search"
+                required
+                value={searchResult}
+                // type={showPassword ? "text" : "password"}
+                onChange={(e) => setSearchResult(e.target.value)}
+              />
             </div>
-            <Button type="submit" variant="">Search</Button>
-            <Button type="button" onClick={()=>{
-              setSearchResult("")
-             return  dispatch(getAllParticipants(currentPage, limit)).then((res) => {
-                return true;
-              });
-            }} variant="" disabled={!searchResult}>Clear</Button>
+            <Button type="submit" variant="">
+              Search
+            </Button>
+            <Button
+              type="button"
+              onClick={() => {
+                setSearchResult("");
+                return dispatch(getAllParticipants(currentPage, limit)).then(
+                  (res) => {
+                    return true;
+                  }
+                );
+              }}
+              variant=""
+              disabled={!searchResult}
+            >
+              Clear
+            </Button>
           </form>
         </div>
         <div className="flex justify-center items-center">
@@ -135,219 +138,130 @@ export const Participantlist = () => {
           </div>
         </div>
       </div>
-      {
-        partcipantList.length == 0 ? <div className="text-center m-5">No result found !!</div>
-       : <table className="w-full min-w-max table-auto text-left">
-       <thead>
-         <tr>
-           {tableHead.map((head) => (
-             <th
-               key={head}
-               className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
-             >
-               <Typography
-                 variant="small"
-                 color="blue-gray"
-                 className="font-normal leading-none opacity-70"
-               >
-                 {head}
-               </Typography>
-             </th>
-           ))}
-         </tr>
-       </thead>
-       <tbody>
-         {partcipantList?.map((el, index) => {
-           const isLast = index === partcipantList.length - 1;
-           const classes = isLast ? "p-4" : "p-4 border-b border-blue-gray-50";
+      {partcipantList.length == 0 ? (
+        <div className="text-center m-5">No result found !!</div>
+      ) : (
+        <table className="w-full min-w-max table-auto text-left">
+          <thead>
+            <tr>
+              {tableHead.map((head) => (
+                <th
+                  key={head}
+                  className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+                >
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="font-normal leading-none opacity-70"
+                  >
+                    {head}
+                  </Typography>
+                </th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {partcipantList?.map((el, index) => {
+              const isLast = index === partcipantList.length - 1;
+              const classes = isLast
+                ? "p-4"
+                : "p-4 border-b border-blue-gray-50";
 
-           return (
-             <tr key={el._id}>
-               <td className={classes}>
-                <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                   {el.name || "-"}
-                 </Typography>
-               </td>
-               <td className={classes}>
-                 <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                   {el.email || "-"}
-                 </Typography>
-               </td>
-               <td className={classes}>
-                 <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                   {el.participantType || "-"}
-                 </Typography>
-               </td>
-               <td className={classes}>
-                 <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                   {el.gender || "-"}
-                 </Typography>
-               </td>
-               <td className={classes}>
-                 <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                  {
-                    cohortList?.map((pl)=>{
-                      if (el.cohort == pl._id){
-                        return pl.name || "NA"
-                      }
-                    })
-                  }
-                 </Typography>
-               </td>
-               <td className={classes}>
-                 <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                   {convertDateFormat(el.dob.split("T")[0]) || "-"}
-                 </Typography>
-               </td>
-               <td className={classes}>
-                 <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                   {el.address.addressLine.substring(0, 20).length < el.address.addressLine.length
-                      ? el.address.addressLine.substring(0, 20) + "..."
-                      : el.address.addressLine || "-"}
-                 </Typography>
-               </td>
-               <td className={classes}>
-                 <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                   {el.phone || "-"}
-                 </Typography>
-               </td>
-               <td className={classes}>
-                 <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                   {el.address.state || "-"}
-                 </Typography>
-               </td>
-               <td className={classes}>
-                 <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                   {el.address.city || "-"}
-                 </Typography>
-               </td>
-               <td className={classes}>
-                 <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                   {el.address.pincode || "-"}
-                 </Typography>
-               </td>
-               <td className={classes}>
-                 <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                   {el.emergencyContact.name || "-"}
-                 </Typography>
-               </td>
-               <td className={classes}>
-                 <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                   {el.emergencyContact.phone || "-"}
-                 </Typography>
-               </td>
-               <td className={classes}>
-                 <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                   {el.emergencyContact.relationship || "-"}
-                 </Typography>
-               </td>
-               <td className={classes}>
-                 <Typography
-                   variant="small"
-                   color="blue-gray"
-                   className="font-normal"
-                 >
-                   {convertDateFormat(el.createdAt.split("T")[0]) || "-"}
-                 </Typography>
-               </td>
-               {/* <td className={classes}>
-               <Typography
-                 as="a"
-                 href="#"
-                 variant="small"
-                 color="blue-gray"
-                 className="font-medium border w-[100px] text-center p-1 rounded-lg bg-maincolor text-white"
-               >
-                 See deatails
-               </Typography>
-             </td> */}
-               <td className={classes}>
-                 <Typography
-                   as="a"
-                   href="#"
-                   variant="small"
-                   color="blue-gray"
-                   onClick={() => toggleModal(el)}
-                   className="text-maincolor2 text-[20px]"
-                 >
-                   <CiEdit />
-                 </Typography>
-               </td>
-               {/* <td className={classes}>
-                 <Typography
-                   as="a"
-                   href="#"
-                   variant="small"
-                   color="blue-gray"
-                   className="text-red-500  text-[20px]"
-                 >
-                   <MdOutlineDeleteOutline />
-                 </Typography>
-               </td> */}
-             </tr>
-           );
-         })}
-       </tbody>
-     </table>
-      }
+              return (
+                <tr key={el._id}>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {el.name || "-"}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {el.email || "-"}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {el.participantType || "-"}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {el.gender || "-"}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal"
+                    >
+                      {cohortList?.map((pl) => {
+                        if (el.cohort == pl._id) {
+                          return pl.name || "NA";
+                        }
+                      })}
+                      {cohortList?.filter((pl) => el.cohort == pl._id)?.length
+                        ? null
+                        : "NA"}
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      as="a"
+                      href="#"
+                      variant="small"
+                      color="blue-gray"
+                      onClick={() => toggleModalRead(el)}
+                      className="text-maincolor2 text-[20px]"
+                    >
+                      <MdOutlineRemoveRedEye />
+                    </Typography>
+                  </td>
+                  <td className={classes}>
+                    <Typography
+                      as="a"
+                      href="#"
+                      variant="small"
+                      color="blue-gray"
+                      onClick={() => toggleModal(el)}
+                      className="text-maincolor2 text-[20px]"
+                    >
+                      <CiEdit />
+                    </Typography>
+                  </td>
+                  
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
 
       <EditParticipants
+        isOpen={isModalOpen}
+        onClose={toggleModal}
+        name={name}
+        singleParticipant={singleParticipant}
+      />
+
+      <EditParticipants
+        name={name}
         isOpen={isModalOpen}
         onClose={toggleModal}
         singleParticipant={singleParticipant}
