fdae9f765e442257c658fafe8e2ae5f988531141
Hilton-Borah <hiltonborah123@gmail.com>
2024-12-19 11:27:30 +0530

    pie chart added

--- src/Pages/dashboard/allCentreReport.jsx
+++ src/Pages/dashboard/allCentreReport.jsx
@@ -1114,7 +1114,7 @@ export const AllCentreReport = () => {
             }
           </PDFDownloadLink>
         </div>
-        <PDFViewer width={600} height={800}>
+        {/* <PDFViewer width={600} height={800}>
           <MyDocument
             cohortList={cohortList}
             cohortSelect={cohortSelect}
@@ -1134,7 +1134,7 @@ export const AllCentreReport = () => {
             des1={des1}
             des2={des2}
           />
-        </PDFViewer>
+        </PDFViewer> */}
       </div>
     </div>
   );
